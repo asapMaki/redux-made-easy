92200de606eb48b38f275480ba012dee2b5bb4e2
Mahir Kadić <mahir.kadic@edu.fit.ba>
2019-04-11 02:40:20 +0200

    Project structre improved

--- reduxHelpers/App.js
+++ reduxHelpers/App.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import { Provider }                     from 'react-redux';
 import { PersistGate }                  from 'redux-persist/integration/react';
 import { persistor, store }             from './src/store';
+import { createRootNavigator }          from "./src/routers/index";
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    return (
+      const Layout = createRootNavigator();
+      return (
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <Layout />
---
+++ reduxHelpers/src/containers/Movies/index.js
@@ -0,0 +1,25 @@
+import React, { Component }                                               from 'react';
+import MoviesComponent                                                    from "../../components/Movies/index";
+import { connect }                                                        from "react-redux";
+import { setModal }                                                       from "../../store/actions";
+import {withNavigation}                                                   from 'react-navigation';
+
+class MoviesContainer extends Component {
+    render() {
+        let props = this.props;
+        return (
+            <MoviesComponent {...props}/>
+        );
+    }
+}
+
+function mapStateToProps( state ) {
+    return {
+        user: state.user,
+        modal: state.modal
+    }
+}
+
+export default connect( mapStateToProps, {
+    setModal,
+} )(withNavigation(CardsContainer));
