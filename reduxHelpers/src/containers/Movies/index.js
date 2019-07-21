f475fa5be8bf596efa81b7b714546fab1546e431
Mahir Kadić <mahir.kadic@edu.fit.ba>
2019-07-21 20:49:25 +0200

    Structure added for tvShows and movies

--- reduxHelpers/src/containers/Movies/index.js
+++ reduxHelpers/src/containers/Movies/index.js
@@ -1,25 +1,26 @@
-import React, { Component }                                               from 'react';
-import MoviesComponent                                                    from "../../components/Movies/index";
-import { connect }                                                        from "react-redux";
-import { setModal }                                                       from "../../store/actions";
-import {withNavigation}                                                   from 'react-navigation';
+import React, { Component } from 'react';
+import Movies from '../../components/Movies/index';
+import { connect } from 'react-redux';
+import { setModal } from '../../store/actions';
+import { withNavigation } from 'react-navigation';
 
 class MoviesContainer extends Component {
     render() {
         let props = this.props;
-        return (
-            <MoviesComponent {...props}/>
-        );
+        return <Movies {...props} />;
     }
 }
 
-function mapStateToProps( state ) {
+function mapStateToProps(state) {
     return {
         user: state.user,
-        modal: state.modal
-    }
+        modal: state.modal,
+    };
 }
 
-export default connect( mapStateToProps, {
-    setModal,
-} )(withNavigation(MoviesContainer));
+export default connect(
+    mapStateToProps,
+    {
+        setModal,
+    },
+)(withNavigation(MoviesContainer));
